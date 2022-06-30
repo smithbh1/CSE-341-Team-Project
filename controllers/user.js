c6aceb75b05f9a25425d0b1974a429ca3ce815d7
rodsam9 <sr_odriguez9@yahoo.com>
2022-06-30 17:48:00 -0600

    user get and post

--- controllers/user.js
+++ controllers/user.js
@@ -4,25 +4,57 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
     // #swagger.tags = ['User']
-    
+    User
+        .find()
+        .then((data) => {
+            if (data) {
+                res.send(data);
+                console.log(data);
+            }
+            else {
+                res.status(404).send({ message: 'No data to display.' });
+            }
+        })
 };
 
-const getSingle = async (req,res,next) => {
+const getSingle = async (req, res, next) => {
+    // #swagger.tags = ['User']
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Use a valid user id');
+    }
+    const userId = new ObjectId(req.params.id);
+    User
+        .find({ _id: userId })
+        .toArray((err, result) => {
+            if (err) {
+                res.status(400).json({ message: err });
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(result[0]);
+        });
+};
+
+const addOne = async (req, res, next) => {
+    // #swagger.tags = ['User']
+    const userInfo = new Goal(req.body);
+    userInfo.save()
+        .then((data) => {
+            console.log(data);
+            res.status(201).send(data);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || 'Error occured while making user contact info.'
+            });
+        });
+};
+
+const editOne = async (req, res, next) => {
     // #swagger.tags = ['User']
 
 };
 
-const addOne = async (req,res,next) => {
-    // #swagger.tags = ['User']
-
-};
-
-const editOne = async (req,res,next) => {
-    // #swagger.tags = ['User']
-
-};
-
-const deleteOne = async (req,res,next) => {
+const deleteOne = async (req, res, next) => {
     // #swagger.tags = ['User']
 
 };
