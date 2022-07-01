b4e03e3bfb09fbbd45082bc26c735d8c5e6106b3
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-07-01 09:41:14 -0600

    diet get and push

--- controllers/diet.js
+++ controllers/diet.js
@@ -2,20 +2,57 @@ const db = require('../models')
 const Diet = db.diets;
 const ObjectId = require('mongodb').ObjectId;
 
-
 const getAll = async (req, res, next) => {
     // #swagger.tags = ['Diets']
+    Diet
+    .find()
+    .then((data) => {
+        if(!data) res.status(404).send({message:'Nothing to Display'});
+        else {
+            res.send(data);
+            console.log(data);
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: 'Error retrieving Diet',
+            error: err
+        });
+    });
 };
 
 const getSingle = async (req,res,next) => {
     // #swagger.tags = ['Diets']
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use a valid contact id to find a contact.');
+      }
+      const userId = new ObjectId(req.params.id);
+      Diet
+      .find({_id: userId})
+      .toArray((err, result) => {
+        if (err){
+            res.status(400).json({message: err});
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result[0]);
+      });
 
 };
 
 const addOne = async (req,res,next) => {
     // #swagger.tags = ['Diets']
+    const diet = new Diet(req.body);
+    diet.save()
+    .then((data) => {
+        console.log(data);
+        res.status(201).send(data);
+    })
+    .catch((err) =>{
+        res.status(500).send({
+            message: err.message || 'Some error occurred while creating the diet.'});
+    });
 
-};
+        };
 
 const editOne = async (req,res,next) => {
     // #swagger.tags = ['Diets']
