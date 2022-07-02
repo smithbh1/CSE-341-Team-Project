9ed268d80d300111e55730598c00378ca6366c5d
ejgdr <gui20006@byui.edu>
2022-07-01 23:06:40 -0600

    t

--- swagger.js
+++ swagger.js
@@ -2,25 +2,25 @@ const swaggerAutogen = require('swagger-autogen')();
 
 
 // // Local test environment
-const doc = {
-  info: {
-    title: 'Health App API',
-    description: 'Create a workout health plan and track your progress with this health application'
-  },
-  host: 'localhost:8080',
-  schemes: ['http']
-};
+// const doc = {
+//   info: {
+//     title: 'Health App API',
+//     description: 'Create a workout health plan and track your progress with this health application'
+//   },
+//   host: 'localhost:8080',
+//   schemes: ['http']
+// };
 
 // Heroku test environment
 
-// const doc = {
-//     info: {
-//       title: 'Health App API',
-//       description: 'Create a workout health plan and track your progress with this health application'
-//     },
-//     host: 'health-tracking-app.herokuapp.com',
-//     schemes: ['https']
-//   };
+const doc = {
+    info: {
+      title: 'Health App API',
+      description: 'Create a workout health plan and track your progress with this health application'
+    },
+    host: 'health-tracking-app.herokuapp.com',
+    schemes: ['https']
+  };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
