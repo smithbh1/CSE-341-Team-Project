378ec2044bcbfc3ba9c0cd94bfa568d7cafc9483
ejgdr <gui20006@byui.edu>
2022-07-02 00:28:13 -0600

    Plans: GetAll, Post, GetById

--- controllers/plan.js
+++ controllers/plan.js
@@ -1,60 +1,62 @@
 const db = require('../models')
 const Plan = db.plans;
-const ObjectId = require('mongodb').ObjectId;
+// const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res, next) => {
+module.exports.getAll = (req, res) => {
     // #swagger.tags = ['Plan']
-    Plan
-    .find()
-    .then((data) => {
-        if(!data) {
-            res.status(404).send({message:'No plans to display'})
-        } else {
+    try {
+        Plan.find({})
+        .then((data) => {
             res.send(data);
             console.log(data);
-        }
-    }).catch((err) => {
-        res.status(500).send({message: 'Error retrieving your plan', error: err});
-    });
-};
-
-const getSingle = async (req,res,next) => {
-    // #swagger.tags = ['Plan']
-    if(!ObjectId.isValid(req.params.id)) {
-        res.status(404).json('Must use a valid id to find the right plan');
+        }).catch((err) => {
+            res.status(500).send({message: err.message || 'Error retrieving your plan'});
+        });
+    } catch (err) {
+        res.status(500).json(err);
     }
-    const planId = new ObjectId(req.params.id);
-    Plan
-    .find({ _id: planId })
-    .toArray((err, result) => {
-        if (err) {
-            res.status(400).json({ message: err });
-        }
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(result[0]);
-    });
 };
 
-const addOne = async (req,res,next) => {
+module.exports.getSingle = (req,res) => {
     // #swagger.tags = ['Plan']
-    const plan = new Plan(req.body);
-    plan.save()
-    .then((data) => {
-        console.log(data);
-        res.status(200).send(data);
-    }).catch((err) => {
-        res.status(500).send({message: err.message || 'Some error ocurred creating your plan'});
-    });
+    try {
+        const planId = req.params.id;
+        Plan.find({ _id: planId })
+        .then((data) => {
+            res.status(200).send(data);
+          })
+          .catch((err) => {
+            res.status(500).send({
+              message: err.message || 'Some error occurred while retrieving your plan.'
+            });
+          });
+    } catch (err) {
+        res.status(500).json(err);
+    } 
 };
 
-const editOne = async (req,res,next) => {
+module.exports.addOne = (req,res) => {
+    // #swagger.tags = ['Plan']
+    try {
+        const plan = new Plan(req.body);
+        plan.save()
+        .then((data) => {
+            console.log(data);
+            res.status(201).send(data);
+        }).catch((err) => {
+            res.status(500).send({message: err.message || 'Some error ocurred while creating your plan'});
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+module.exports.editOne = (req,res,next) => {
     // #swagger.tags = ['Plan']
 
 };
 
-const deleteOne = async (req,res,next) => {
+module.exports.deleteOne =(req,res,next) => {
     // #swagger.tags = ['Plan']
 
 };
-
-module.exports = { getAll, getSingle, addOne, editOne, deleteOne }
